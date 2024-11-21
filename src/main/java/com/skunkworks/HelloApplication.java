8cdbd0d686024b25d2541428a5d0dcea11594b2c
Yasmin Woodlock <yasminwoodlock@gmail.com>
2024-11-21 15:08:03 +0000

    testing

--- src/main/java/com/skunkworks/HelloApplication.java
+++ src/main/java/com/skunkworks/HelloApplication.java
@@ -3,19 +3,29 @@ package com.skunkworks;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws IOException{
+        Text text1 = new Text("Email");
+        Text text2 = new Text("Password");
+        Text text3 = new Text("3");
+        Text text4 = new Text("f");
+
+        GridPane gp
     }
+//    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
 
     public static void main(String[] args) {
         launch();
