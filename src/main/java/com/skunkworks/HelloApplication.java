02070619f407704282f6f58b7f42829ed784ea0b
Yasmin Woodlock <yasminwoodlock@gmail.com>
2024-11-21 15:19:22 +0000

    adding gridpane to display

--- src/main/java/com/skunkworks/HelloApplication.java
+++ src/main/java/com/skunkworks/HelloApplication.java
@@ -2,8 +2,11 @@ package com.skunkworks;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -12,12 +15,25 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException{
+        stage.setTitle("Bok");
         Text text1 = new Text("Email");
         Text text2 = new Text("Password");
         Text text3 = new Text("3");
         Text text4 = new Text("f");
 
-        GridPane gp
+        GridPane gp = new GridPane();
+        gp.getColumnConstraints().add(new ColumnConstraints(10));
+        gp.setGridLinesVisible(true);
+
+        gp.setMinSize(400, 200);
+        gp.setPadding(new Insets(10, 10, 10, 10));
+
+        gp.setVgap(5);
+        gp.setHgap(5);
+
+        Scene scene = new Scene(gp);
+        stage.setScene(scene);
+        stage.show();
     }
 //    public void start(Stage stage) throws IOException {
 //        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
