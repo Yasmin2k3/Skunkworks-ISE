df75fc6cdc3b2ee51f69b42250a457c1c0919612
Yasmin Woodlock <yasminwoodlock@gmail.com>
2024-11-26 17:30:48 +0000

    added binary sort algorithm

--- src/main/java/com/skunkworks/HelloApplication.java
+++ src/main/java/com/skunkworks/HelloApplication.java
@@ -39,6 +39,41 @@ public class HelloApplication extends Application {
         return arr;
     }
 
+    //algorithm gotten from geeks for geeks
+    //TODO: mid will make the grid rectangle yellow
+    private int binarySearch(ArrayList<Integer> arr, int x, GridPane gp)
+    {
+        int low = 0, high = arr.size() - 1;
+        while (low <= high) {
+            Rectangle rec = new Rectangle(cellWidth-5, cellHeight-5);
+            rec.setFill(Color.rgb(232, 215, 86));
+            rec.setStroke(Color.rgb(204, 183, 47));
+            rec.setStrokeWidth(2);
+//
+//            GridPane.setRowIndex(stackPane, row);
+//            GridPane.setColumnIndex(stackPane, col);
+//            gp.getChildren().add(stackPane);
+
+            int mid = low + (high - low) / 2;
+
+            // Check if x is present at mid
+            if (arr.get(mid) == x)
+                return mid;
+
+            // If x greater, ignore left half
+            if (arr.get(mid) < x)
+                low = mid + 1;
+
+                // If x is smaller, ignore right half
+            else
+                high = mid - 1;
+        }
+
+        // If we reach here, then element was
+        // not present
+        return -1;
+    }
+
     @Override
     public void start(Stage stage) throws IOException{
         stage.setTitle("Bok");
@@ -55,13 +90,11 @@ public class HelloApplication extends Application {
 
         for (int row=0; row<rowNum; row++){
             for (int col=0; col<colNum; col++){
-                System.out.println("index: " + (col + (10*row)));
+                //System.out.println("index: " + (col + (10*row)));
                 String textContents = Integer.toString(sort.get(col + (10*row)));
                 Text text = new Text(textContents);
                 text.setFill(Color.BLACK);
                 text.setFont(Font.font("Calibri", FontWeight.BOLD, FontPosture.REGULAR,12));
-                text.setX(30);
-                text.setY(30);
 
                 Rectangle rec = new Rectangle(cellWidth-5, cellHeight-5);
                 rec.setFill(Color.rgb(91, 127, 255));
@@ -71,6 +104,7 @@ public class HelloApplication extends Application {
                 StackPane stackPane = new StackPane();
                 stackPane.getChildren().addAll(rec, text);
 
+                //use this for updating the colours:
                 GridPane.setRowIndex(stackPane, row);
                 GridPane.setColumnIndex(stackPane, col);
                 gp.getChildren().add(stackPane);
@@ -78,6 +112,11 @@ public class HelloApplication extends Application {
         }
 
         gp.setAlignment(Pos.CENTER);
+
+        int randomNum = sort.get((int) (Math.random() * sort.size()));
+
+        System.out.println("Finding: " + randomNum);
+        System.out.println(binarySearch(sort, randomNum, gp));
 
         Scene scene = new Scene(gp, 800, 600);
         stage.setScene(scene);
