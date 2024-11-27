79dcaefda390ff16b6bef6964107c4cd5f1c286c
Yasmin Woodlock <yasminwoodlock@gmail.com>
2024-11-27 14:40:19 +0000

    added button to kick off binary search

--- src/main/java/com/skunkworks/HelloApplication.java
+++ src/main/java/com/skunkworks/HelloApplication.java
@@ -1,24 +1,30 @@
 package com.skunkworks;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+
+//TODO: Add stackpane with grid and then label after?
 
 public class HelloApplication extends Application {
 
@@ -40,33 +46,40 @@ public class HelloApplication extends Application {
     }
 
     //algorithm gotten from geeks for geeks
-    //TODO: mid will make the grid rectangle yellow
+    //TODO: remove yellow rectangles if they already exist
     private int binarySearch(ArrayList<Integer> arr, int x, GridPane gp)
     {
+        Rectangle rec = new Rectangle(cellWidth-5, cellHeight-5);
+        rec.setFill(Color.rgb(232, 215, 86));
+        rec.setStroke(Color.rgb(204, 183, 47));
+        rec.setStrokeWidth(2);
         int low = 0, high = arr.size() - 1;
         while (low <= high) {
-            Rectangle rec = new Rectangle(cellWidth-5, cellHeight-5);
-            rec.setFill(Color.rgb(232, 215, 86));
-            rec.setStroke(Color.rgb(204, 183, 47));
-            rec.setStrokeWidth(2);
-//
-//            GridPane.setRowIndex(stackPane, row);
-//            GridPane.setColumnIndex(stackPane, col);
-//            gp.getChildren().add(stackPane);
 
             int mid = low + (high - low) / 2;
+//            System.out.println(arr.indexOf(arr.get(mid))/10);
+//            System.out.println(arr.indexOf(arr.get(mid))%10);
+            GridPane.setRowIndex(rec, arr.indexOf(arr.get(mid))/10); //gets the 10ths values
+            GridPane.setColumnIndex(rec, arr.indexOf(arr.get(mid))%10);//gets the first digit
+            //System.out.println(gp.getChildren());
+            gp.getChildren().add(rec);
 
             // Check if x is present at mid
-            if (arr.get(mid) == x)
+            if (arr.get(mid) == x){
                 return mid;
-
+            }
             // If x greater, ignore left half
-            if (arr.get(mid) < x)
+            else if (arr.get(mid) < x) {
+
                 low = mid + 1;
+                gp.getChildren().remove(rec);
+            }
 
                 // If x is smaller, ignore right half
-            else
+            else {
                 high = mid - 1;
+                gp.getChildren().remove(rec);
+            }
         }
 
         // If we reach here, then element was
@@ -74,12 +87,8 @@ public class HelloApplication extends Application {
         return -1;
     }
 
-    @Override
-    public void start(Stage stage) throws IOException{
-        stage.setTitle("Bok");
+    private GridPane makeGrid(ArrayList<Integer> sort){
         GridPane gp = new GridPane();
-        ArrayList<Integer> sort = makeList();
-        System.out.println(sort);
 
         for (int col=0; col<colNum; col++){
             gp.getColumnConstraints().add(new ColumnConstraints(cellWidth));
@@ -92,8 +101,7 @@ public class HelloApplication extends Application {
             for (int col=0; col<colNum; col++){
                 //System.out.println("index: " + (col + (10*row)));
                 String textContents = Integer.toString(sort.get(col + (10*row)));
-                Text text = new Text(textContents);
-                text.setFill(Color.BLACK);
+                Label text = new Label(textContents);
                 text.setFont(Font.font("Calibri", FontWeight.BOLD, FontPosture.REGULAR,12));
 
                 Rectangle rec = new Rectangle(cellWidth-5, cellHeight-5);
@@ -101,24 +109,47 @@ public class HelloApplication extends Application {
                 rec.setStroke(Color.rgb(82, 64, 163));
                 rec.setStrokeWidth(2);
 
-                StackPane stackPane = new StackPane();
-                stackPane.getChildren().addAll(rec, text);
-
                 //use this for updating the colours:
-                GridPane.setRowIndex(stackPane, row);
-                GridPane.setColumnIndex(stackPane, col);
-                gp.getChildren().add(stackPane);
+                GridPane.setRowIndex(rec, row);
+                GridPane.setColumnIndex(rec, col);
+                gp.getChildren().add(rec);
+                gp.add(text, col, row);
+                GridPane.setHalignment(text, HPos.CENTER);
             }
         }
 
         gp.setAlignment(Pos.CENTER);
+        return gp;
+    }
 
-        int randomNum = sort.get((int) (Math.random() * sort.size()));
 
-        System.out.println("Finding: " + randomNum);
-        System.out.println(binarySearch(sort, randomNum, gp));
+    @Override
+    public void start(Stage stage) throws IOException{
+        stage.setTitle("Bok");
+        ArrayList<Integer> sort = makeList();
+        System.out.println(sort);
 
-        Scene scene = new Scene(gp, 800, 600);
+        GridPane gp = makeGrid(sort);
+
+        //adding button
+        Button button = new Button("Search for a random number");
+        button.setDefaultButton(true);
+        //make button do something on action:
+        EventHandler<ActionEvent> event = new EventHandler<>() {
+            public void handle(ActionEvent e) {
+                int randomNum = sort.get((int) (Math.random() * sort.size()));
+                System.out.println("Finding: " + randomNum);
+                binarySearch(sort, randomNum, gp);
+            }
+        };
+        button.setOnAction(event);
+
+        //displays everything on screen in a nice format
+        VBox display = new VBox(8);
+        display.getChildren().addAll(button, gp);
+        display.setAlignment(Pos.TOP_CENTER);
+
+        Scene scene = new Scene(display, 800, 600);
         stage.setScene(scene);
         stage.show();
     }
